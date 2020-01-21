6092047d37be372d55cd6d1f5d0ac4dc1bf7be2a
Frederick Morin <frederickmorin18@gmail.com>
2020-01-21 07:29:10 -0500

    Navigate between welcome and info using state machine

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { Styled } from 'theme-ui';
+import { Button } from '@theme-ui/components';
+import { useMachine } from '@xstate/react';
+import { formMachine } from '../src/formMachine';
+
+export default () => {
+  const [current, send] = useMachine(formMachine);
+
+  switch (current.value) {
+    case 'welcome':
+      return (
+        <>
+          <Styled.h1>Welcome</Styled.h1>
+          <Button onClick={() => send({ type: 'START', lang: 'en' })}>
+            Continue in English
+          </Button>
+          <Button onClick={() => send({ type: 'START', lang: 'en' })}>
+            Continuer en français
+          </Button>
+        </>
+      );
+
+    case 'info':
+      return <Styled.h1>Info</Styled.h1>;
+
+    default:
+      return null;
+  }
+};
