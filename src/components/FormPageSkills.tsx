deb00045a59e0be40257194aab36989f3a0f0896
Frederick Morin <frederickmorin18@gmail.com>
2020-02-11 16:42:02 -0500

    Implement FormPageSkills with ConditionalCheckboxes

--- src/components/FormPageSkills.tsx
+++ src/components/FormPageSkills.tsx
@@ -1,10 +1,18 @@
 import * as yup from 'yup';
 import FormPageLayout from './FormPageLayout';
-import { Text } from '@theme-ui/components';
-import { useContext } from 'react';
-import { useService } from '@xstate/react';
-import { ServiceContext } from '../pages';
+import { Divider, Flex, Label, Checkbox } from '@theme-ui/components';
+// import { useContext } from 'react';
+// import { useService } from '@xstate/react';
+// import { ServiceContext } from '../pages';
 import isEqual from 'lodash/isEqual';
+import {
+  Form,
+  FormInputChoice,
+  FormField,
+  FormInputChoiceBox,
+} from './FormComponents';
+import FormNavButton from './FormNavButton';
+import { useFormContext, Controller } from 'react-hook-form';
 
 type Categories = 'kitchen' | 'maintenance' | 'technology' | 'accounting';
 type SelectedSkills = Partial<{ [K in Categories]: Record<string, boolean> }>;
@@ -17,6 +25,7 @@ export const skillSchema = yup.object().shape({
   ),
   skills: yup
     .mixed<SelectedSkills>()
+    .notRequired()
     .test('skills', 'custom error message', function(
       this: yup.TestContext,
       skills: SelectedSkills
@@ -41,19 +50,113 @@ export const skillSchema = yup.object().shape({
 export type Skills = yup.InferType<typeof skillSchema>;
 
 const FormPageSkills = () => {
-  const service = useContext(ServiceContext);
-  const [state] = useService(service);
+  // const service = useContext(ServiceContext);
+  // const [state, send] = useService(service);
+
+  const onSubmit = (data: Skills) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    // send({
+    //   type: 'NEXT',
+    //   data: {
+    //     skills: data,
+    //   },
+    // });
+  };
 
   return (
     <FormPageLayout
       title="Interests and skills"
-      description="Start by choosing your areas of interests"
+      description="Choose your areas of interests in Dhamma service and select your skills if applicable"
     >
-      <Text sx={{ fontFamily: 'mono' }}>
-        <pre>{JSON.stringify(state.context, null, 2)}</pre>
-      </Text>
+      <Form<Skills> onSubmit={onSubmit}>
+        {/* validationSchema={skillSchema} */}
+        <FormInputChoice
+          type="checkbox"
+          label="Areas of interest"
+          name="categories"
+          options={[
+            { label: 'Kitchen', value: 'kitchen' },
+            { label: 'Maintenance', value: 'maintenance' },
+            { label: 'Technology', value: 'technology' },
+            { label: 'Accounting', value: 'accouting' },
+          ]}
+        />
+
+        <ConditionalCheckboxes />
+
+        <FormField label="Other skills" name="other" />
+
+        <FormInputChoice
+          type="checkbox"
+          name="consent"
+          options={[
+            {
+              label: 'I consent to being contacted by committees',
+            },
+          ]}
+        />
+
+        <input type="submit" />
+
+        <Divider mx={0} mt={0} mb={4} />
+        <Flex
+          sx={{
+            justifyContent: 'flex-end',
+          }}
+        >
+          <FormNavButton />
+        </Flex>
+      </Form>
     </FormPageLayout>
   );
+};
+
+const skillsData = {
+  kitchen: [
+    'question 1',
+    'question 2',
+    'question 3',
+    'question 4',
+    'question 5',
+    'question 6',
+    'question 7',
+    'question 8',
+  ],
+  maintenance: [
+    'painting',
+    'plumbing',
+    'carpentry',
+    'electricity',
+    'architecture',
+    'etc',
+  ],
+  technology: ['sysadmin', 'software development', 'tech support', 'etc'],
+  accounting: ['accounting', 'bookkeeping', 'etc'],
+};
+
+const ConditionalCheckboxes = () => {
+  const { watch, control, register } = useFormContext<Skills>();
+
+  if (watch('categories')?.includes('kitchen')) {
+    return (
+      <Controller
+        as={FormInputChoiceBox}
+        name="skills.kitchen"
+        label="Kitchen questions"
+        control={control}
+      >
+        {skillsData.kitchen.map((question, i) => (
+          <Label key={question}>
+            <Checkbox name={`skills.kitchen[${question}]`} ref={register} />
+            {question}
+          </Label>
+        ))}
+      </Controller>
+    );
+  }
+
+  return null;
 };
 
 export default FormPageSkills;
