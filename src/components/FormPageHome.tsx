299959dbcc250c7cd5167ee3d998e7084efa82ba
Frederick Morin <frederickmorin18@gmail.com>
2020-02-29 10:26:30 -0500

    Remove FormPageLayout form home page

--- src/components/FormPageHome.tsx
+++ src/components/FormPageHome.tsx
@@ -1,27 +1,46 @@
-import { Button } from '@theme-ui/components';
-import FormPageLayout from './FormPageLayout';
+import { Button, Text, Box } from '@theme-ui/components';
 import { useFormService } from '../hooks/useFormService';
+import { Styled } from 'theme-ui';
 
 const FormPageHome = () => {
   const [, send] = useFormService();
 
   return (
-    <FormPageLayout title="Home" description="this is a test description">
-      <>
-        <Button
-          variant="form"
-          onClick={() => send({ type: 'START', lang: 'en' })}
-        >
-          Continue in English
-        </Button>
-        <Button
-          variant="form"
-          onClick={() => send({ type: 'START', lang: 'fr' })}
-        >
-          Continuer en français
-        </Button>
-      </>
-    </FormPageLayout>
+    <Box
+      sx={{
+        textAlign: 'center',
+      }}
+    >
+      <Styled.h1>Dhamma Suttama</Styled.h1>
+      <Text
+        as="p"
+        sx={{
+          fontSize: 2,
+        }}
+      >
+        Formulaire sur vos intérêts et disponibilités pour le service
+      </Text>
+      <Text
+        as="p"
+        sx={{
+          fontSize: 2,
+        }}
+      >
+        Service interest and availability form
+      </Text>
+      <Button
+        variant="form"
+        onClick={() => send({ type: 'START', lang: 'en' })}
+      >
+        Continue in English
+      </Button>
+      <Button
+        variant="form"
+        onClick={() => send({ type: 'START', lang: 'fr' })}
+      >
+        Continuer en français
+      </Button>
+    </Box>
   );
 };
 
